1594e09e8bf3038ff84bc17a80f29c23991e9067
Dave Juanes <dmjuanes_@hotmail.es>
2024-11-21 07:28:32 -0400

    [UPDATE] RECIPE DETAIL WITH ONTAP CARDS AND SEND DATA TO ANOTHER WIDGET

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_book/screens/recipe_detail.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -38,45 +39,50 @@ class HomeScreen extends StatelessWidget {
   }
 
   Widget _RecipesCard(BuildContext context){
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: 125,
-            child: Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    height: 125,
-                    width: 100,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(12),
-                      // child: Image.asset('assets/images/your_image.png', fit: BoxFit.cover,),
-                      child: Image.network('https://images.unsplash.com/photo-1669941060931-6912c4538ba3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHJvYWR8ZW58MHx8MHx8fDA%3D', fit: BoxFit.cover,),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 26,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Road', style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),),
-                      SizedBox(height: 4,),
-                      Container(
-                        height: 2,
-                        width: 75,
-                        color: Colors.blueGrey,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => RecipeDetail(recipeName: 'Road One')));
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 125,
+              child: Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      height: 125,
+                      width: 100,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        // child: Image.asset('assets/images/your_image.png', fit: BoxFit.cover,),
+                        child: Image.network('https://images.unsplash.com/photo-1669941060931-6912c4538ba3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHJvYWR8ZW58MHx8MHx8fDA%3D', fit: BoxFit.cover,),
                       ),
-                      Text('Dave Juanes', style: TextStyle(fontSize: 16, fontFamily: 'Quicksand')),
-                      SizedBox(height: 4,),                      
-                    ],
-                  )
-                ],
+                    ),
+                    SizedBox(
+                      width: 26,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Road', style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),),
+                        SizedBox(height: 4,),
+                        Container(
+                          height: 2,
+                          width: 75,
+                          color: Colors.blueGrey,
+                        ),
+                        Text('Dave Juanes', style: TextStyle(fontSize: 16, fontFamily: 'Quicksand')),
+                        SizedBox(height: 4,),                      
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
+      ),
     );
   }
 }
